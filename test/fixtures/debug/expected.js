5254bc32837ae991318b67711f45dc48035dd645
Guy Bedford <guybedford@gmail.com>
2019-07-25 00:22:53 -0400

    globalThis global detection support

--- test/fixtures/debug/expected.js
+++ test/fixtures/debug/expected.js
@@ -2,7 +2,7 @@ import { dew as _msDew } from "ms";
 var exports = {},
     _dewExec = false;
 
-var _global = typeof self !== "undefined" ? self : global;
+var _global = typeof globalThis !== "undefined" ? globalThis : typeof self !== "undefined" ? self : global;
 
 export function dew() {
   if (_dewExec) return exports;
--- test/fixtures/when/expected.js
+++ test/fixtures/when/expected.js
@@ -13,7 +13,7 @@ import { dew as _applyDew } from "./lib/apply";
 var exports = {},
     _dewExec = false;
 
-var _global = typeof self !== "undefined" ? self : global;
+var _global = typeof globalThis !== "undefined" ? globalThis : typeof self !== "undefined" ? self : global;
 
 export function dew() {
   if (_dewExec) return exports;
