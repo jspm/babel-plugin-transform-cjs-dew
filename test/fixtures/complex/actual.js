05521c3b96e69b18f753093bcc3540498c99f42b
Guy Paterson-Jones <guy.paterson.jones@gmail.com>
2023-04-02 03:30:20 +0200

    bug: add test case for complex.js parent scope reference bug (#17)

---
+++ test/fixtures/complex/actual.js
@@ -0,0 +1,7 @@
+(function() {
+  if (typeof define === 'function' && define['amd']) {
+  } else if (typeof exports === 'object') {
+    Object.defineProperty(Complex, "__esModule", { 'value': true });
+    Complex['default'] = Complex;
+  }
+})();
