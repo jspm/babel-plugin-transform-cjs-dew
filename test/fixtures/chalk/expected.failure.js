1b3f09f32364c1a87c2f4eb255584494aef6bf9b
Guy Bedford <guybedford@gmail.com>
2023-08-20 17:33:03 -0700

    remove failure

--- test/fixtures/chalk/expected.failure.js
+++
@@ -1,5 +0,0 @@
-import _process from "process";
-var process = _process;
-// Example of a use of the "process" global that isn't picked up in chalk@2.4.2:
-const isSimpleWindowsTerm = process.platform === "win32" && !(process.env.TERM || "").toLowerCase().startsWith("xterm");
-export default {};
